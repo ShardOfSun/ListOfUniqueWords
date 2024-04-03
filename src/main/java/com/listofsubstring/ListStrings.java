ee0a18fb7d96d7d696fd7f318f14848cd6c43ac5
ShardOfSun <ternovskayaliza2004@gmail.com>
2024-04-03 09:50:38 +0300

    Created class ListStrings with GPT #4

--- src/main/java/com/listofsubstring/ListStrings.java
+++ src/main/java/com/listofsubstring/ListStrings.java
@@ -1,7 +1,52 @@
 package main.java.com.listofsubstring;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ListStrings {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private Node head;
+
+    private static class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void add(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    public void loadFromFile(String filename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("File reading error: " + e.getMessage());
+        }
     }
 }
